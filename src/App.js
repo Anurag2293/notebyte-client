4e999d02570f20710903c85e018695c118a9d9a5
Anurag Dhote <anuragdhote392@gmail.com>
2023-01-11 19:08:13 +0530

    Setup Note Context using useContext() Hook

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <NoteState>
+        <Navbar />
 
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/about' element={<About/>}/>
-      </Routes>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </NoteState>
     </>
   );
 }
