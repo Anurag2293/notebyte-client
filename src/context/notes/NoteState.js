91c22aff98dc38129dd9028e1beaa7f41c557a68
Anurag Dhote <anuragdhote392@gmail.com>
2023-01-11 19:46:19 +0530

    Toggling active link of Navbar using useLocation hook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,26 +1,8 @@
 import NoteContext from "./noteContext";
-import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name" : "Harry",
-        "class" : "5b",
-        age : 10
-    }
-
-    const [state, setState] = useState(s1)
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name" : "Larry",
-                "class" : "10b",
-                age : 15
-            })
-        }, 1000);
-    }
-
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider> 
     )
